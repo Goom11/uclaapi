5b8d878d78ac932630e8bdbdc554427e792f54f6
dastardlylemon <jwu@efunda.com>
2014-01-30 22:45:31 -0800

    created api endpoints

--- uclaapi.py
+++ uclaapi.py
@@ -7,3 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'hello werld'
+
+@app.route('/api/v1/classes')
+def get_class_list():
+    return 'classes'
+
+@app.route('/api/v1/classes/<int:class_id>')
+def get_class_info(class_id):
+    return 'class info'
+
+@app.route('/api/v1/textbooks')
+def get_book_list():
+    return 'books'
+
+@app.route('/api/v1/textbooks/<int:book_id>')
+def get_book_info(book_id):
+    return 'book info'
