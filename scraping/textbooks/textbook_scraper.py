ea806a1d1e7ddfe398d49f079ab82148123fbee6
lowellbander <Lowell@s3051-176.resnet.ucla.edu>
2014-02-02 00:38:53 -0800

    getting closer

--- scraping/textbooks/textbook_scraper.py
+++ scraping/textbooks/textbook_scraper.py
@@ -1,22 +1,35 @@
 from bs4 import BeautifulSoup
 
 def single():
-    soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/single_textbook.html"))
+    soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/courses/single_course.html"))
     
     title = str(soup.tbody.a.text).strip()
     
     bar = soup.findAll('div', id=lambda x: x and x.endswith('SKU'))
     SKU = str(bar[0].text).strip()[5:]
     
-    textbook = {
+    course = {
         "title": title,
         "SKU": SKU
     }
     
-    print textbook
+    print course
+
+def multiple():
+    soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/courses/multiple_courses.html"))
+    tag = BeautifulSoup('<b class="boldest">Extremely bold</b>')
+    tag = tag.b
+    course_soup = soup.find(id="ctl00_PageContent_pnlPrintMode")
+    courses = []
+    for thing in course_soup:
+        if type(thing) == type(tag):
+            courses.append(thing)
+    print "courses:"
+    print len(courses)
 
 def main():
-    single()
+    #single()
+    multiple()
 
 if __name__ == "__main__":
         main()
