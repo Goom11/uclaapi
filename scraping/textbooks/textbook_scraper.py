e35818d718c781817e7987a885d8ba4d464207b5
lowellbander <Lowell@s3051-176.resnet.ucla.edu>
2014-02-02 00:42:54 -0800

    got careless with global replace lol

--- scraping/textbooks/textbook_scraper.py
+++ scraping/textbooks/textbook_scraper.py
@@ -1,22 +1,22 @@
 from bs4 import BeautifulSoup
 
 def single():
-    soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/courses/single_course.html"))
+    soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/textbooks/single_textbook.html"))
     
     title = str(soup.tbody.a.text).strip()
     
     bar = soup.findAll('div', id=lambda x: x and x.endswith('SKU'))
     SKU = str(bar[0].text).strip()[5:]
     
-    course = {
+    textbook = {
         "title": title,
         "SKU": SKU
     }
     
-    print course
+    print textbook 
 
 def multiple():
-    soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/courses/multiple_courses.html"))
+    soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/textbooks/multiple_textbooks.html"))
     tag = BeautifulSoup('<b class="boldest">Extremely bold</b>')
     tag = tag.b
     course_soup = soup.find(id="ctl00_PageContent_pnlPrintMode")
