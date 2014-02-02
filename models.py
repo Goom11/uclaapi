6beecaed1b7e5b4866b42a33290cb03393d4b8e6
Aman Agarwal <aman.agarwal.2718@gmail.com>
2014-02-02 01:04:09 -0800

    defined docs

--- models.py
+++ models.py
@@ -1,7 +1,12 @@
 from uclaapi import db
 
 class Class(db.Document):
-   name = db.StringField(max_length=255, required=True)
+   course_name = db.StringField(max_length=255, required=True, unique=True)
+   instructor = db.StringField(max_length=255, required=True, unique=True)
+   books = db.ListField(ReferenceField(Textbook))
 
 class Textbook(db.Document):
-   name = db.StringField(max_length=255, required=True)
+   title = db.StringField(max_length=255, required=True)
+   SKU = db.StringField(max_length=255, required=True)
+   new_price = db.DecimalField(min_value=0)
+   used_price = db.DecimalField(min_value=0)
