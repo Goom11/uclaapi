2d8f202fcb4418c2244a508317ebc58a8d1eb71e
Aman Agarwal <aman.agarwal.2718@gmail.com>
2014-02-02 10:11:01 -0800

    fixed style issues 2 lines before functions removed trailing spaces

--- textbook_scraper.py
+++ textbook_scraper.py
@@ -2,20 +2,22 @@ from ...models import *
 from bs4 import BeautifulSoup
 import sys
 
+
 def single():
     soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/textbooks/single_textbook.html"))
-    
+
     title = str(soup.tbody.a.text).strip()
-    
+
     bar = soup.findAll('div', id=lambda x: x and x.endswith('SKU'))
     SKU = str(bar[0].text).strip()[5:]
-    
+
     textbook = {
         "title": title,
         "SKU": SKU
     }
-    
-    print textbook 
+
+    print textbook
+
 
 def multiple():
     soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/textbooks/multiple_textbooks.html"))
@@ -29,22 +31,26 @@ def multiple():
     print "courses:"
     print len(courses)
 
+
 def create_course_from_tag(tag):
     #TODO
     return
 
+
 def get_course_list_from_soup(soup):
     return course_list
+
 
 def main():
     #single()
     #multiple()
     soup = BeautifulSoup((open(sys.argv[1])))
-    print "Opening %s..." % sys.argv[1]  
+    print "Opening %s..." % sys.argv[1]
     return
     course_list = get_course_list_from_soup(soup)
     for course in course_list:
         create_course_from_tag(course)
+
 
 if __name__ == "__main__":
         main()
