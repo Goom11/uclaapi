8546b8cf4040fe90f19df3ff970987a7b1632863
Lowell Bander <Lowell@wifi-131-179-8-137.host.ucla.edu>
2014-02-03 01:33:05 -0800

    removed old/deprecated functions

--- textbook_scraper.py
+++ textbook_scraper.py
@@ -2,29 +2,6 @@
 from bs4 import BeautifulSoup
 import sys
 
-def single():
-    soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/textbooks/single_textbook.html"))
-    title = str(soup.tbody.a.text).strip()
-    bar = soup.findAll('div', id=lambda x: x and x.endswith('SKU'))
-    SKU = str(bar[0].text).strip()[5:]
-    textbook = {
-        "title": title,
-        "SKU": SKU
-    }
-    print textbook
-
-def multiple():
-    soup = BeautifulSoup(open("/Users/Lowell/gits/uclaapi/scraping/textbooks/multiple_textbooks.html"))
-    tag = BeautifulSoup('<b class="boldest">Extremely bold</b>')
-    tag = tag.b
-    course_soup = soup.find(id="ctl00_PageContent_pnlPrintMode")
-    courses = []
-    for thing in course_soup:
-        if type(thing) == type(tag):
-            courses.append(thing)
-    print "courses:"
-    print len(courses)
-
 def get_tags_from_soup(soup):
     tag = BeautifulSoup('<b></b>')
     tag = tag.b
@@ -49,8 +26,10 @@ def create_course_from_tag(tag):
         print "SKU: [%s]" % SKU 
         prices = book.find('div',
                 {"class":"addcartform"}).find('div').findAll('div')
-        for price in prices:
-            print "PRICE: %r" % float(price.span.text[1:])
+        new_price = float(prices[0].span.text[1:])
+        old_price = float(prices[1].span.text[1:])
+        print "new_price: [%f]" % new_price
+        print "old_price: [%f]" % old_price
     return
 
 def get_course_list_from_soup(soup):
