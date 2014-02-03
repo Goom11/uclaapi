eb066c5024b6697c63d0e74f271d17ea0839e60b
Aman Agarwal <aman.agarwal.2718@gmail.com>
2014-02-03 00:05:32 -0800

    pep8

--- scraper.py
+++ scraper.py
@@ -9,14 +9,14 @@ r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data)
 
-for link in soup.find_all("a", { "class" : "main" }):
+for link in soup.find_all("a", {"class": "main"}):
     coursetitle = link.contents[0]
     intourl = 'http://www.registrar.ucla.edu/catalog/' + link.get('href')
     # print(url + "/" + link.get('href') + "\n" + coursetitle)
     soup2 = BeautifulSoup(requests.get(intourl).text)
-    for link2 in soup2.find_all("a", { "class" : "nav-landing-menu" }):
+    for link2 in soup2.find_all("a", {"class": "nav-landing-menu"}):
         if 'Course Listings' in link2.contents[0]:
             courseDescriptUrl = 'http://www.registrar.ucla.edu/catalog/' + link2.get('href')
             soup3 = BeautifulSoup(requests.get(courseDescriptUrl).text)
-            for link3 in soup3.find_all("p", { "class" : "coursebody" }):
+            for link3 in soup3.find_all("p", {"class": "coursebody"}):
                 print(link3)
