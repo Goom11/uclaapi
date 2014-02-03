bb0d8bce8ebd5559e9af4a4b3ea1ad60d23b9e51
Lowell Bander <Lowell@wifi-131-179-8-71.host.ucla.edu>
2014-02-03 14:27:18 -0800

    do work

--- dining_hours_scraper.py
+++ dining_hours_scraper.py
@@ -3,10 +3,20 @@
 # TODO: save hours in database (run daily, perhaps get hrs of future dates)
 # TODO: wrap with API
 
+import urllib2
+from bs4 import BeautifulSoup
+
 def main():
-    print "hello werld"
-    url =
-    "https://secure5.ha.ucla.edu/restauranthours/dining-hall-hours-by-day.cfm"
+    url = "https://secure5.ha.ucla.edu/restauranthours/dining-hall-hours-by-day.cfm"
+    response = urllib2.urlopen(url)
+    html = response.read()
+    soup = BeautifulSoup(html)
+    table = soup.find('table', {"border":"1"})
+    rows = table.findAll('tr')
+    date = rows[0].td.strong.text.strip()
+    print "date: [%s]" % date
+    print rows[1]
+    #brkfst, lunch, dinner, late night
 
 if __name__ == "__main__":
     main()
