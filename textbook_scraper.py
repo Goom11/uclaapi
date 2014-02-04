c3d86698b2eab44913914879401be64902b916cc
Lowell Bander <Lowell@s3250-185.resnet.ucla.edu>
2014-02-03 16:50:20 -0800

    handles case where course has no textbook

--- textbook_scraper.py
+++ textbook_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 import sys
 
-# TODO : handle the case where a course has no textbooks
+# TODO : handle case where textbooks is only available in new/used
 # TODO : implement save method
 
 def get_tags_from_soup(soup):
@@ -24,18 +24,22 @@ def create_coursedict_from_tag(tag):
     coursedict['instructor'] = str(tag.find('div',
         {"class":"coursenotes"}).text.strip()[11:])
     coursedict['books'] = []
-    textbooks = tag.find('ul').find('div', {"class":"Products"}).table.tbody
-    textbooks = get_tags_from_soup(textbooks)
-    for book in textbooks:
-        bookdict = {}
-        bookdict['title'] = str(book.a.text.strip())
-        bookdict['SKU'] = str(book.find('div', id=lambda x: x and
-                x.endswith('SKU')).text.strip()[5:])
-        prices = book.find('div',
-                {"class":"addcartform"}).find('div').findAll('div')
-        bookdict['new_price'] = float(prices[0].span.text[1:])
-        bookdict['old_price'] = float(prices[1].span.text[1:])
-        coursedict['books'].append(bookdict)
+    print coursedict
+    try:
+        textbooks = tag.find('ul').find('div', {"class":"Products"}).table.tbody
+        textbooks = get_tags_from_soup(textbooks)
+        for book in textbooks:
+            bookdict = {}
+            bookdict['title'] = str(book.a.text.strip())
+            bookdict['SKU'] = str(book.find('div', id=lambda x: x and
+                    x.endswith('SKU')).text.strip()[5:])
+            prices = book.find('div',
+                    {"class":"addcartform"}).find('div').findAll('div')
+            bookdict['new_price'] = float(prices[0].span.text[1:])
+            bookdict['old_price'] = float(prices[1].span.text[1:])
+            coursedict['books'].append(bookdict)
+    except AttributeError:
+        print "no textbooks found for %r" % coursedict
     return coursedict
 
 def get_course_list_from_soup(soup):
