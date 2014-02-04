3ff9691d669353aae00d4afdaf922dbbcd5a4864
Lowell Bander <Lowell@s3250-185.resnet.ucla.edu>
2014-02-03 23:06:14 -0800

    better naming

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ class Textbook(db.Document):
    new_price = db.DecimalField(min_value=0)
    used_price = db.DecimalField(min_value=0)
 
-class Class(db.Document):
-   course_name = db.StringField(max_length=255, required=True, unique=True)
+class Course(db.Document):
+   name = db.StringField(max_length=255, required=True, unique=True)
    instructor = db.StringField(max_length=255, required=True, unique=True)
    books = db.ListField(db.ReferenceField(Textbook))
 
