e49a47eb9d540dedbeb00f68fe2a2b6d51514f39
Lowell Bander <Lowell@s86-101.resnet.ucla.edu>
2014-02-06 13:12:49 -0800

    added tab for readability

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from models import *
 
-print "hello werld"
 for course in Course.objects:
     print "course: %s, instructor: %s" % (course.name, course.instructor)
     for book in course.books:
-        print "title: %s, SKU: %s" % (book.title, book.SKU)
+        print "     title: %s, SKU: %s" % (book.title, book.SKU)
