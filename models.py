44d1830c8d676a123e12b7b1d985f4b41e4bebdb
Lowell Bander <Lowell@Lowells-MacBook-Air-2.local>
2014-02-13 22:24:32 -0800

    more complete Hours model

--- models.py
+++ models.py
@@ -5,96 +5,84 @@ from uclaapi import db
 ###### Textbooks/Registrar ###### 
 
 class Textbook(db.Document):
-   title = db.StringField(max_length=255, required=True)
-   SKU = db.StringField(max_length=255, required=True)
-   new_price = db.DecimalField(min_value=0)
-   used_price = db.DecimalField(min_value=0)
+    title = db.StringField(max_length=255, required=True)
+    SKU = db.StringField(max_length=255, required=True)
+    new_price = db.DecimalField(min_value=0)
+    used_price = db.DecimalField(min_value=0)
 
-# this object is getting heavy, but projection should take care of that
 class Course(db.Document):
-   # TODO: parse 'name' to separate fields!
-   name = db.StringField(max_length=255, required=True, unique=True)
-   description = db.StringField(required=True)
-   department = db.StringField(max_length=255, required=True, unique=True) #eg Math
-   number = db.StringField(max_length=255, required=True, unique=True) #eg 33A
-   quarter = db.StringField(max_length=255, required=True, unique=True) #eg Winter
-   year = db.DecimalField(min_value=0) #eg 2014
-   # TODO: move instructor to lecture
-   instructor = db.StringField(max_length=255, required=True, unique=True)
-   books = db.ListField(db.ReferenceField(Textbook))
-   prerequisites = db.ListField(db.ReferenceField(Course))
-   units = db.DecimalField((min_value=0)
-   # grading detail, GE status, impacted class, enrollment restriction, 
+    # TODO: parse 'name' to separate fields!
+    name = db.StringField(max_length=255, required=True, unique=True)
+    description = db.StringField(required=True)
+    department = db.StringField(max_length=255, required=True, unique=True) 
+    number = db.StringField(max_length=255, required=True, unique=True)
+    quarter = db.StringField(max_length=255, required=True, unique=True) 
+    year = db.DecimalField(min_value=0) 
+    # TODO: move instructor to lecture
+    instructor = db.StringField(max_length=255, required=True, unique=True)
+    books = db.ListField(db.ReferenceField(Textbook))
+    prerequisites = db.ListField(db.ReferenceField(Course))
+    units = db.DecimalField((min_value=0)
+    lectures = db.ListField(db.ReferenceField(Lecture))
+    # grading detail, GE status, impacted class, enrollment restriction, 
 
 class Lecture(db.Document): # subclasses Course, has specific instructor & classroom number?
    number = db.StringField(max_length=255, required=True, unique=True) #eg 3
    professor = db.ReferenceField(Instructor)
    location = db.ReferenceField(Room)
    date_of_final = db.DateTimeField()
-   #time = db.ReferenecField(Hours)
+   hours = db.ListField(db.ReferenceField(Hour))
+   sections = db.ListField(db.ReferenceField(Section))
 
-class Section(db.Document): # subclasses Lecture, has specific TA & classroom number
-   section = db.StringField(max_length=255, required=True, unique=True) #eg C
+class Section(db.Document): 
+   letter = db.StringField(max_length=255, required=True, unique=True)
    ta = db.ReferenceField(Instructor)
    location = db.ReferenceField(Room)
    enrollment = db.IntField((min_value=0)
-   # total enrollment = sum(lecture.section.enrollment)
-   # needs to update itself ridiculously frequently to be useful. perhaps a
-   # separate script will run continuously to update this field. is that fast
-   # enough? given how many classes there are, perhaps we should be running
-   # multiple instances of the script in parallel to be verily up-to-date
-   # hella server load lol
-   #time = db.ReferenecField(Hours)
+   hours = db.ListField(db.ReferenceField(Hour))
 
 class Instructor(db.Document):
    first_name = db.StringField(max_length=255)
-   last_name = db.StringField(max_length=255, required=True) #can't be unique b/c multiple profs with same same, so there needs to be some alternative pk
-   #type = ta|professor # stringfield doesn't seem concrete enough. can we create a data type that only has two values? boolean? what if we want three later on?
+   last_name = db.StringField(max_length=255, required=True) 
+   # last_name can't be unique b/c multiple profs with same same, 
+   # so there needs to be some alternative pk
+   # type = ta|professor # stringfield doesn't seem concrete enough. can we create a data type that only has two values? boolean? what if we want three later on?
 
 ###### Dining ###### 
 
 class Restaurant(db.Document):
-   name = db.StringField(max_length=255, required=True, unique=True)
-   Menus = db.ListField(db.ReferenceField(Menu))
-   #Hours = 
+    name = db.StringField(max_length=255, required=True, unique=True)
+    menus = db.ListField(db.ReferenceField(Menu))
+    hours = db.ListField(db.ReferenceField(Hour))
 
 class Menu(db.Document):
-   date = db.DateTimeField(required=True) 
-   foods = db.ListField(db.ReferenceField(Food))
+    date = db.DateTimeField(required=True) 
+    foods = db.ListField(db.ReferenceField(Food))
 
 class Food(db.Document):
-   name = db.StringField(max_length=255, required=True, unique=True)
-   ingredients = db.ListField(db.StringField(max_length=255))
+    name = db.StringField(max_length=255, required=True, unique=True)
+    ingredients = db.ListField(db.StringField(max_length=255))
 
-class Hours(db.Document):
-   date = db.DateTimeField(required=True) 
-   # some fancy way to handle varying meal periods and breaks within meal periods
-   # would be cool to use this same model for Library?
-   # this data is going to be repeated a lot though.
-   # perhaps other models should point to this model instead of creating a new
-   # one if this one alreadt exists?
-   # for this model to be useful to meal periods, the relation between meal
-   # periods and and Hours should be bijective? right? because that would allow
-   # for this to be a heurisitic of how many swipes you can use before the
-   # week's over?
-   # then there are things like the library hours which have different hours for
-   # different parts of the library. :/
+class Hour(db.Document):
+    name = db.StringField(max_length=255, required=True, unique=True)
+    start = db.DateTimeField(required=True) 
+    end = db.DateTimeField(required=True) 
 
-# Buildings
+##### Buildings #####
 
 class Building(db.Document):
-    # amenities like room reservations! programatically send emails to reserve rooms lol. 
     name = db.StringField(max_length=255, required=True, unique=True)
     rooms = db.ListField(db.ReferenceField(Room))
-    #geolocationfield! Flask says it doesn't support this field. does that mean
-    # there's no way we can use it?
+    amenities = db.ListField(db.StringField())
+    #geolocationfield! Flask says it doesn't support this field. 
+    # does that mean there's no way we can use it?
 
 class Room(db.Document):
-    number = db.StringField(max_length=255, required=True, unique=True) #eg 2236, A103B
-    # GeoLocationField once we have made svg layovers of each building!
-    # construct these from those little maps in the building
+    number = db.StringField(max_length=255, required=True, unique=True) 
     max_occupancy = db.IntField(min_value=0)
+    # svg layover
 
 class Library(db.Document):
-    #hours
+    name = db.StringField(max_length=255, required=True, unique=True)
+    hours = db.ListField(db.ReferenceField(Hour))
 
