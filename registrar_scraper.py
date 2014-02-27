8508e85d4ffb592cf29ba1d06a0a60ed930c2094
Lowell Bander <Lowell@wifi-131-179-1-207.host.ucla.edu>
2014-02-27 13:41:33 -0800

    preliminary dict generation
    needs to be generalized to work for all cases!

--- registrar_scraper.py
+++ registrar_scraper.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 import requests
+from pprint import pprint
+import re
 from bs4 import BeautifulSoup
 
 client = MongoClient()
@@ -8,6 +10,14 @@ url = 'http://www.registrar.ucla.edu/catalog/catalog-curricul.htm'
 r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data)
+
+def get_title_dict(title):
+    title_dict = {}
+    words = title.split('.')
+    title_dict['number'] = str(words[0])
+    title_dict['title'] = str(words[1].strip())
+    title_dict['units'] = int(re.findall('\d+', words[2])[0])
+    return title_dict
 
 def main():
     for link in soup.find_all("a", {"class": "main"}):
@@ -20,9 +30,12 @@ def main():
                 courseDescriptUrl = 'http://www.registrar.ucla.edu/catalog/' + link2.get('href')
                 soup3 = BeautifulSoup(requests.get(courseDescriptUrl).text)
                 for link3 in soup3.find_all("p", {"class": "coursebody"}):
-                    print(link3)
                     if type(link3) == type(BeautifulSoup('<b></b>').b):
-                        print link3
+                        foo = link3
+                        title = foo.span.get_text().strip()
+                        title_dict = get_title_dict(title)
+                        pprint(title_dict)
+                        course = {}
                         return
 
 if __name__ == "__main__":
