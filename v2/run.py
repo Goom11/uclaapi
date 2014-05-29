54f5fc46f47b5be71a55edb4bebb3a349ca4b1de
Lowell Bander <lowellbander@gmail.com>
2014-05-29 13:36:24 -0700

    works!

--- v2/run.py
+++ v2/run.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.mongorest import MongoRest
@@ -12,5 +14,26 @@ app.debug = True
 app.config.update(
     MONGODB_HOST = 'localhost',
     MONGODB_PORT = '27017',
-    MONGO_DB = 'kinetic',
+    MONGODB_DB = 'chimera',
 )
+
+db = MongoEngine(app)
+api = MongoRest(app)
+
+class Course(db.Document):
+    title = db.StringField()
+
+class CourseResource(Resource):
+    document = Course
+
+@api.register(name='course', url='/course/')
+class CourseView(ResourceView):
+    resource = CourseResource
+    methods = [methods.Create, methods.Update, methods.Fetch, methods.List, methods.Delete]
+
+
+if __name__ == '__main__':
+    app.run()
+    #port = int(os.environ.get('PORT', 8000))
+    #app.run(host='0.0.0.0', port=port)
+
